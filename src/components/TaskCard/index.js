81e3ecf05dc89c329ece092feb3502b2686ff736
oagid <ooagid@gmail.com>
2021-04-21 14:44:58 -0300

    finsh components web

---
+++ src/components/TaskCard/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as S from './styles';
+
+
+function TaskCard() {
+    return (
+        <S.Container>
+            <S.TopCard>
+                <img src="{iconDefault}" alt="Icone da Tarefa" />
+                <h3>Titulo da Tarefa</h3>
+            </S.TopCard>
+            <S.BottomCard>
+                <strong>17/10/2020</strong>
+                <span>10:00</span>
+            </S.BottomCard>
+        </S.Container>
+    )
+}
+
+export default TaskCard;
